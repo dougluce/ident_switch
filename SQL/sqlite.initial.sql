ea21509f2a93aba366c7b493b664f9a5375131bc
Boris Gulay <boris@gulay.name>
2018-10-12 17:32:24 +0300

    Added initial script for SQLite. Thanx to Gergely Papp. Closes #29.

---
+++ SQL/sqlite.initial.sql
@@ -0,0 +1,38 @@
+CREATE TABLE ident_switch
+(
+	id
+		serial
+		PRIMARY KEY,
+	user_id
+		integer
+		NOT NULL
+		REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	iid
+		integer
+		NOT NULL
+		REFERENCES identities(identity_id) ON DELETE CASCADE ON UPDATE CASCADE
+		UNIQUE,
+	username
+		varchar(64),
+	password
+		varchar(64),
+	host
+		varchar(64),
+	port
+		integer
+		CHECK(port > 0 AND port <= 65535),
+	delimiter
+		char(1),
+	label
+		varchar(32),
+	flags
+		integer
+		NOT NULL
+		DEFAULT(0),
+	UNIQUE (user_id, label)
+);
+CREATE INDEX IX_ident_switch_user_id
+ON
+	ident_switch(user_id);
+CREATE INDEX IX_ident_switch_iid on ident_switch(iid);
+
