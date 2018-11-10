cc68ab15219d77f20b8257e483bf4b0e76c49b43
Boris Gulay <boris@gulay.name>
2018-11-10 18:32:44 +0300

    JS optimizations.

--- ident_switch-form.js
+++ ident_switch-form.js
@@ -8,3 +8,34 @@ $(function() {
 	plugin_switchIdent_processPreconfig();
 });
 
+function plugin_switchIdent_processPreconfig() {
+    var disFld = $("INPUT[name='_ident_switch.form.readonly']");
+    disFld.parentsUntil("TABLE", "TR").hide();
+
+    var disVal = disFld.val();
+    if (disVal > 0) {
+        $("INPUT[name='_ident_switch.form.host']").prop("disabled", true);
+        $("SELECT[name='_ident_switch.form.secure']").prop("disabled", true);
+        $("INPUT[name='_ident_switch.form.port']").prop("disabled", true);
+    }
+    if (2 == disVal) {
+        $("INPUT[name='_ident_switch.form.username']").prop("disabled", true);
+    }
+
+}
+
+function plugin_switchIdent_enabled_onChange(e) {
+    var $enFld = $("INPUT[name='_ident_switch.form.enabled']");
+    $("INPUT[name!='_ident_switch.form.enabled'], SELECT", $enFld.parents("FIELDSET")).prop("disabled", !$enFld.is(":checked"));
+    plugin_switchIdent_processPreconfig();
+}
+
+function plugin_switchIdent_secure_onChange(e) {
+    var $secSel = $("SELECT[name='_ident_switch.form.secure']");
+    var $portFld = $("INPUT[name='_ident_switch.form.port']");
+
+    if ('SSL' === $secSel.val().toUpperCase())
+        $portFld.attr("placeholder", 993);
+    else
+        $portFld.attr("placeholder", 143);
+}
--- ident_switch-switch.js
+++ ident_switch-switch.js
@@ -16,11 +16,7 @@ $(function() {
 	}
 
 	if (isOk)
-		$sw.show();
-
-	$("INPUT[name='_ident_switch.form.enabled']").change();
-	$("SELECT[name='_ident_switch.form.secure']").change();
-	plugin_switchIdent_processPreconfig();
+        $sw.show();
 });
 
 function plugin_switchIdent_addCbLarry($sw) {
@@ -47,39 +43,9 @@ function plugin_switchIdent_addCbClassic($sw) {
 	return false;
 }
 
-function plugin_switchIdent_processPreconfig() {
-	var disFld = $("INPUT[name='_ident_switch.form.readonly']");
-	disFld.parentsUntil("TABLE", "TR").hide();
-
-	var disVal = disFld.val();
-	if (disVal > 0) {
-		$("INPUT[name='_ident_switch.form.host']").prop("disabled", true);
-		$("SELECT[name='_ident_switch.form.secure']").prop("disabled", true);
-		$("INPUT[name='_ident_switch.form.port']").prop("disabled", true);
-	}
-	if (2 == disVal) {
-		$("INPUT[name='_ident_switch.form.username']").prop("disabled", true);
-	}
-
-}
-
-function plugin_switchIdent_enabled_onChange(e) {
-	var $enFld = $("INPUT[name='_ident_switch.form.enabled']");
-	$("INPUT[name!='_ident_switch.form.enabled'], SELECT", $enFld.parents("FIELDSET")).prop("disabled", !$enFld.is(":checked"));
-	plugin_switchIdent_processPreconfig();
-}
-
-function plugin_switchIdent_secure_onChange(e) {
-	var $secSel = $("SELECT[name='_ident_switch.form.secure']");
-	var $portFld = $("INPUT[name='_ident_switch.form.port']");
-
-	if ('SSL' === $secSel.val().toUpperCase())
-		$portFld.attr("placeholder", 993);
-	else
-		$portFld.attr("placeholder", 143);
-}
-
 function plugin_switchIdent_switch(val) {
+    rcmail.env.unread_counts = {};
+    console.log(rcmail.env.unread_counts);
 	rcmail.http_post('plugin.ident_switch.switch', { '_ident-id': val, '_mbox': rcmail.env.mailbox });
 }
 
