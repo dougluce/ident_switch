6ba63dfe50ae86376fd2824c43a084f89f3c92fb
Boris Gulay <boris@gulay.name>
2019-01-08 15:26:04 +0300

    Updated MySQL scripts to work with new SMTP feature. Closes #34 Closes #26

--- SQL/mysql.initial.sql
+++ SQL/mysql.initial.sql
@@ -14,19 +14,22 @@ CREATE TABLE IF NOT EXISTS ident_switch
 		varchar(64),
 	password
 		varchar(64),
-	host
+	imap_host
 		varchar(64),
-	port
+	imap_port
 		int
 		CHECK(port > 0 AND port <= 65535),
-	delimiter
-		char(1),
 	label
 		varchar(32),
 	flags
 		int
 		NOT NULL
 		DEFAULT 0,
+  smtp_host
+    varchar(64),
+  smtp_port
+    int
+    CHECK(port > 0 AND port <= 65535),
 	UNIQUE KEY user_id_label (user_id, label),
 	CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT fk_identity_id FOREIGN KEY (iid) REFERENCES identities(identity_id) ON DELETE CASCADE ON UPDATE CASCADE,
