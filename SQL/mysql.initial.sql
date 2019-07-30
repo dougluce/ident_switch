652d1aaa1f4553d989ae0b580becd3200a4c266e
Boris Gulay <boris@gulay.name>
2019-07-30 23:14:30 +0300

    Fix field names in CHECK. Thanx Tina Rosenstein

--- SQL/mysql.initial.sql
+++ SQL/mysql.initial.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS ident_switch
 		varchar(64),
 	imap_port
 		int
-		CHECK(port > 0 AND port <= 65535),
+		CHECK(imap_port > 0 AND imap_port <= 65535),
 	label
 		varchar(32),
 	flags
@@ -29,12 +29,12 @@ CREATE TABLE IF NOT EXISTS ident_switch
     varchar(64),
   smtp_port
     int
-    CHECK(port > 0 AND port <= 65535),
-	UNIQUE KEY user_id_label (user_id, label),
-	CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT fk_identity_id FOREIGN KEY (iid) REFERENCES identities(identity_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY(id),
-	INDEX IX_ident_switch_user_id (user_id),
-	INDEX IX_ident_switch_iid (iid)
+    CHECK(smtp_port > 0 AND smtp_port <= 65535),
+  UNIQUE KEY user_id_label (user_id, label),
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT fk_identity_id FOREIGN KEY (iid) REFERENCES identities(identity_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY(id),
+  INDEX IX_ident_switch_user_id (user_id),
+  INDEX IX_ident_switch_iid (iid)
 );
 
