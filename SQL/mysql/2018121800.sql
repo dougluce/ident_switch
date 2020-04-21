312026b059a60c6551c69850b2eb69384d61cf83
Boris Gulay <boris@gulay.name>
2020-04-21 21:29:42 +0300

    Quote all identifier for MySQL/MariaDB. Closes #54

--- SQL/mysql/2018121800.sql
+++ SQL/mysql/2018121800.sql
@@ -1,29 +1,29 @@
 ALTER TABLE
-  ident_switch
+  `ident_switch`
 CHANGE
-  host
-  imap_host varchar(64);
+  `host`
+  `imap_host` varchar(64);
 
 ALTER TABLE
-  ident_switch
+  `ident_switch`
 CHANGE
-  port
-  imap_port int;
+  `port`
+  `imap_port` int;
 
 ALTER TABLE
-  ident_switch
+  `ident_switch`
 ADD COLUMN
-	smtp_host
+	`smtp_host`
     varchar(64);
 
 ALTER TABLE
-  ident_switch
+  `ident_switch`
 ADD COLUMN
-  smtp_port
+  `smtp_port`
 		int
-		CHECK(smtp_port > 0 AND smtp_port <= 65535);
+		CHECK(`smtp_port` > 0 AND `smtp_port` <= 65535);
 
 ALTER TABLE
-  ident_switch
+  `ident_switch`
 DROP COLUMN
-  delimiter;
+  `delimiter`;
